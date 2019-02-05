b2a36d6e058c1bc62ba916d50978f80e0a963215
Michal Juraska <mjuraska@fhcrc.org>
2019-02-05 14:05:22 -0800

    Revised 'testIndepTimeMark' (currently only for a univariate mark)

--- R/testIndepTimeMark.R
+++ R/testIndepTimeMark.R
@@ -1,4 +1,4 @@
-### Kolmogorov-Smirnov-type test of conditional independence between T and V given Z
+ ### Kolmogorov-Smirnov-type test of conditional independence between T and V given Z
 
 ####################################################################################
 ### Description: R functions to perform the test of independence between T and V ###
@@ -10,7 +10,7 @@
 ####################################################################################
 
 # T: survival time
-# V: mark
+# U: mark
 # C: censoring time
 # data: replicates of [X=min(T,C), Delta=I(T<=C), Y=U*Delta]
 
@@ -18,97 +18,78 @@
 #        tu[,2] each row corresponds to the coordinates (t,u) of a point
 # output: a vector of the same length as tu, cdf values at (t,u)
 
-
-### Internal Functions
-
-# 'estCDF' returns a vector containing the estimated CDF values at each (T, V) in 'timeMark'.
-# 'timeMark' is a dataframe where each row is a case (i.e., failure) and columns 'T' and 'V' 
-# specify the failure times and mark values, respectively. If calculating a marginal 
-# distribution, the values of one variable are set to infinity.
-# 'data' is a dataframe where each row is a subject. The following columns must be included:
-#   'X' for the right-censored failure times (i.e., min of failure time and censoring time)
-#   'd' for the failure indicator, 1 if failure, 0 if censored
-#   'Y' for the values of the mark variable, set to 0 if no mark is observed
-estCDF <- function(timeMark,data){
-  ord <- order(data$X,-data$d)
-  size <- length(data$X)
+ftu <- function(tu, data){
+  ord <- order(data[, 1], -data[, 2])
+  size <- length(data[, 1])
   prob <- numeric(size)
   surv <- 1
   for (i in 1:size){
-    if (data[ord[i],"d"]==1){ prob[ord[i]] <- surv/(size-i+1) }
-    surv <- surv-prob[ord[i]]
+    if (data[ord[i], 2]==1){ prob[ord[i]] <- surv / (size - i + 1) }
+    surv <- surv - prob[ord[i]]
   }
-  cdf <- sapply(1:NROW(timeMark), function(j){
-    sum(prob[data$X <= timeMark[j,"T"] & data$Y <= timeMark[j,"V"]],na.rm=TRUE)
-  })
+  cdf <- sapply(1:NROW(tu), function(j){ sum(prob[data[, 1] <= tu[j, 1] & data[, 3] <= tu[j, 2]], na.rm=TRUE) })
   return(cdf)
 }
 
-# 'testStat' returns the test statistic
-# 'timeMark' is a dataframe where each row is a case (i.e., failure) and columns 'T' and 'V' 
-# specify the failure times and mark values, respectively
-# 'data' is a dataframe where each row is a subject. The following columns must be included:
-#   'X' for the right-censored failure times
-#   'd' for the failure indicator, 1 if failure, 0 if censored
-#   'Y' for the values of the mark variable, set to 0 if no mark is observed
-testStat <- function(timeMark, data){
-  timeMark <- na.omit(timeMark)
-  return(max(abs(estCDF(timeMark,data) - estCDF(cbind(timeMark$T,Inf),data)*
-                         estCDF(cbind(Inf,timeMark$V),data[data$d==1,]))))
+g <- function(tu, data){
+  if (is.vector(tu)) tu <- t(as.matrix(tu))
+  tu <- na.omit(tu)
+  return(abs(ftu(tu, data) - ftu(cbind(tu[, 1], Inf), data) * ftu(cbind(Inf, tu[, 2]), data[data[, 2]==1, ])))
 }
 
-
-#' Kolmogorov-Smirnov-Type Test of Conditional Independence Between Time to Failure 
-#' and Mark Variable Given Treatment Group
-#' 
-#' \code{KStest} performs a Komogorov-Smirnov-type test on the independence of the 
-#' time to failure, T, and the distribution of the mark variable, V, given the 
-#' treatment group, Z. The independence of T and V given Z is a necessary assumption 
-#' for parameter identifiability in the time-independent density ratio model. 
-#' The function uses a bootstrap algorithm to obtain and return the p-value for the 
-#' test of independence. 
-#' 
-#' @param data a dataframe where each row is a study subject in a given treatment group 
-#' and the following columns are included: \code{X} specifying the right-censored failure 
-#' times, \code{d} specifying the failure indicator (1 if failure, 0 if censored), and 
-#' \code{Y} specifying the values of the mark variable (0 if no mark is observed).
-#' @param iter the number of bootstrap iterations to be performed. Default is 1000.
-#' 
-#' @details 
-#' The null hypothesis is independence between T and V given Z, and the test statistic 
-#' is the supremum of the distance between the joint cdf of (T,V) given Z and the product of 
-#' the conditional marginal cdfs of T and V. Estimation of the joint cdf is based on the 
-#' nonparametric maximum likelihood estimator developed by Huang and Louis (1998).  
-#' The estimated marginal cdf of T is one minums the Kaplan-Meier estimator of the 
-#' conditional survival function of T, and the estimated marginal cdf of V is the 
-#' empirical density function of the observed values of V. A bootstrap algorithm 
-#' is used to estimate the critical values for the distribution under the null. 
-#' 
-#' @return Returns the p-value for the Kolmogorov-Smirnov test of independence between 
-#' T and V given Z. 
-#' 
-#' @examples 
-#' 
+#' Kolmogorov-Smirnov-Type Test of Conditional Independence between the Time-to-Event
+#' and a Univariate Mark Given Treatment
+#'
+#' A nonparametric Komogorov-Smirnov-type test of the null hypothesis that the time-to-event \eqn{T} and a univariate mark \eqn{V} are conditionally independent given treatment \eqn{Z}
+#' as described in Juraska and Gilbert (2013). The conditional independence is a necessary assumption for parameter identifiability in the time-independent density ratio model. A bootstrap
+#' algorithm is used to compute the p-value.
+#'
+#' @param data a data frame restricted to subjects in a given treatment group with the following three columns (in this order): the observed right-censored time to the event of interest,
+#' the event indicator (1 if event, 0 if right-censored), and the univariate mark variable
+#' @param iter the number of bootstrap iterations (1000 by default) used for computing the p-value
+#'
+#' @details
+#' The test statistic is the supremum of the difference between the estimated conditional joint cumulative distribution function (cdf) of \eqn{(T,V)} given \eqn{Z} and the product of
+#' the estimated conditional marginal cdfs of \eqn{T} and \eqn{V} given \eqn{Z}. The joint cdf is estimated by the nonparametric maximum likelihood estimator developed by
+#' Huang and Louis (1998). The marginal cdf of \eqn{T} is estimated as one minus the Kaplan-Meier estimator for the conditional survival function of \eqn{T}, and the marginal
+#' cdf of \eqn{V} is estimated as the empirical cdf of the observed values of \eqn{V}. A bootstrap algorithm is used to compute the p-value.
+#'
+#' @return Returns the bootstrap p-value from the test of conditional independence between \eqn{T} and \eqn{V} given \eqn{Z}.
+#'
+#' @references Juraska, M. and Gilbert, P. B. (2013), Mark-specific hazard ratio model with multivariate continuous marks: an application to vaccine efficacy. \emph{Biometrics} 69(2):328–337.
+#'
+#' Huang, Y. and Louis, T. (1998), Nonparametric estimation of the joint distribution of survival time and mark variables. \emph{Biometrika} 85, 785–798.
+#'
+#' @examples
+#' n <- 500
+#' tx <- rep(0:1, each=n/2)
+#' tm <- c(rexp(n/2, 0.1), rexp(n/2, 0.1 * exp(-0.4)))
+#' cens <- runif(n, 0, 15)
+#' eventTime <- pmin(tm, cens, 3)
+#' eventInd <- as.numeric(tm <= pmin(cens, 3))
+#' mark <- ifelse(eventInd==1, c(rbeta(n/2, 2, 5), rbeta(n/2, 2, 2)), NA)
+#'
+#' testIndepTimeMark(data.frame(eventTime, eventInd, mark)[tx==0, ], iter=20)
+#'
 #' @export
 testIndepTimeMark <- function(data, iter=1000){
-  T <- testStat(data[data$d==1,c("X","Y")], data)
+  D <- max(g(data[data[, 2]==1, c(1, 3)], data))
   n <- NROW(data)
   resamp <- matrix(sample(1:n, n*iter, replace=TRUE), n, iter)
-  obs.mark <- as.vector(data[data$d==1,3])
-  bootT <- sapply(1:iter, function(j){
-    bootData <- data[resamp[,j], c("X","d")]
-    if (sum(bootData$d) > 1){
-      m <- sum(bootData$d)
-      bootMark <- numeric(n)
-      bootMark[which(bootData$d==1)] <- sample(obs.mark, m, replace=TRUE)
-      bootData <- cbind(bootData, bootMark)
-      tstat <- testStat(bootData[bootData$d==1,c(1,3)], bootData)
+  obs.mark <- as.vector(data[data[, 2]==1, 3])
+  bD <- sapply(1:iter, function(j){
+    bdata <- data[resamp[, j], 1:2]
+    if (sum(bdata[, 2]) > 1){
+      m <- sum(bdata[, 2])
+      bmark <- numeric(n)
+      bmark[which(bdata[, 2]==1)] <- sample(obs.mark, m, replace=TRUE)
+      bdata <- cbind(bdata, bmark)
+      tstat <- max(g(bdata[bdata[, 2]==1, c(1,3)], bdata))
     } else {
       tstat <- NULL
     }
-    tstat
+    return(tstat)
   })
-  if (is.list(bootT)){ bootT <- do.call("c", lapply(bootT,"[",1)) }  # if bT is a list, concatenate into a vector
-  bootPval <- mean(bootT >= T)
-  return(bootPval)
+  if (is.list(bD)){ bT <- do.call("c", lapply(bD, "[", 1)) }
+  return(mean(bD >= D))
 }
